34625ab9c020a1eb2f4c0a181db9553c5ad762f0
CrisAriasSarda <127308418+CrisAriasSarda@users.noreply.github.com>
2023-06-14 10:18:21 +0100

    Update RegioneR_Zscore.R

--- RegioneR_Zscore.R
+++ RegioneR_Zscore.R
@@ -6,24 +6,25 @@ library(GenomicFeatures)
 library(regioneR)
 
 # File containing the coordinates of the EBRs and HSBs 
-# Structure: chromosome start end
+# Structure: chromosome start end (only chromosome number: 1 (not chr1))
 tads <- read.table("coordinates_EBRs.txt", sep="", stringsAsFactors = F, header=F)
 
 # File containing the coordinates of: all cattle genes, housekeeping genes, and GC content specific 
-# Structure: chromosome start end
+# Structure: chromosome start end (only chromosome number: 1 (not chr1))
 chip <- read.table("coordinates_cattle_genes.txt", sep="", stringsAsFactors = F, header=F)
 
 # Cattle chromosome size
-# Structure: chromosome start end
+# Structure: chromosome start end (only chromosome number: 1 (not chr1))
 genome <- toGRanges("Cattle_sizes.txt")
 
 pt <- permTest(A=tads, B=chip, ntimes=1000, randomize.function=circularRandomizeRegions,count.once=TRUE,
                evaluate.function=numOverlaps, genome=genome, verbose = TRUE,  force.parallel=FALSE, per.chromosone=FALSE)
 
-# plot of RegioneR
-plot(pt)
-
 lz <- localZScore(pt=pt, A=tads, B=chip)
 
-#plot of local Z score
+
+# plot RegioneR
+plot(pt)
+
+#plot local Z score
 plot(lz)
